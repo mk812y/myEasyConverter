3ccf6766a993d954676da7b7e667cf6c79e411b8
mk812y <mk812y@gmail.com>
2022-10-27 20:03:00 +0300

    Initial Commit

---
+++ myEasyConverter/myEasyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  myEasyConverterApp.swift
+//  myEasyConverter
+//
+//  Created by Михаил Куприянов on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct myEasyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
