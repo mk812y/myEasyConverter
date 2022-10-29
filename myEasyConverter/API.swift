638bc5dbfd9bb973a9b658e83a47f86d2693d22f
mk812y <mk812y@gmail.com>
2022-10-29 18:51:21 +0300

    add testRates

---
+++ myEasyConverter/API.swift
@@ -0,0 +1,27 @@
+//
+//  API.swift
+//  myEasyConverter
+//
+//  Created by Михаил Куприянов on 27.10.2022.
+//
+
+import Foundation
+
+//var semaphore = DispatchSemaphore (value: 0)
+//
+//let url = "https://api.apilayer.com/fixer/convert?to=RUB&from=TRY&amount=10"
+//var request = URLRequest(url: URL(string: url)!,timeoutInterval: Double.infinity)
+//request.httpMethod = "GET"
+//request.addValue("gYDyjOEmdA7p14nB9NhV3hKofFmKXzfR", forHTTPHeaderField: "apikey")
+//
+//let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//  guard let data = data else {
+//    print(String(describing: error))
+//    return
+//  }
+//  print(String(data: data, encoding: .utf8)!)
+//  semaphore.signal()
+//}
+//
+//task.resume()
+//semaphore.wait()
--- myEasyConverter/ContentView.swift
+++ myEasyConverter/ContentView.swift
@@ -10,12 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("dfsdfsdf")
+                Text("dfsdfsdf")
+            }
+            HStack {
+                Text("dfsdfsdf")
+                Text("dfsdfsdf")
+            }
         }
-        .padding()
     }
 }
 
